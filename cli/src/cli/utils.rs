671b70aafabbf203670878431d101f7f5d6499e0
rolv <rolv.apneseth@gmail.com>
2024-11-09 00:05:08 +0000

    tests(cli): cli utils

--- cli/src/cli/utils.rs
+++ cli/src/cli/utils.rs
@@ -10,3 +10,40 @@ pub fn if_supports_colour(
         comfy_table::Color::Reset
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use comfy_table::Color;
+    use termcolor::{ColorChoice, StandardStream};
+
+    #[test]
+    fn test_if_supports_colour() {
+        let get_stream = StandardStream::stdout;
+
+        assert_eq!(
+            comfy_table::Color::Reset,
+            if_supports_colour(&get_stream(ColorChoice::Never), Color::Red)
+        );
+        assert_eq!(
+            comfy_table::Color::Reset,
+            if_supports_colour(&get_stream(ColorChoice::Never), Color::Cyan)
+        );
+
+        assert_eq!(
+            comfy_table::Color::Green,
+            if_supports_colour(&get_stream(ColorChoice::Always), Color::Green)
+        );
+        assert_eq!(
+            comfy_table::Color::Black,
+            if_supports_colour(&get_stream(ColorChoice::AlwaysAnsi), Color::Black)
+        );
+
+        assert_eq!(
+            comfy_table::Color::Reset,
+            if_supports_colour(&get_stream(ColorChoice::Always), Color::Reset)
+        );
+
+        // NOTE: No test for `ColorChoice::Auto` - not sure how that would react in a CI environment
+    }
+}
